28d64504e5b6d3f5fd4fd93b5f63b909db5b6ebe
Bernhard Sirlinger <sirlinger@kk-software.de>
2019-04-12 15:16:19 +0200

    🤡 Add login mockup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,28 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Lartify</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        to="login"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Login</span>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
+  name: 'Lartify',
   components: {
-    HelloWorld
   },
   data () {
     return {
