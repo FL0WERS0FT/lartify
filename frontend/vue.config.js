8932d90cc006820f17da13844c98c05ff4015e03
Bernhard Sirlinger <bernhard@sirlinger.de>
2019-04-23 00:40:32 +0200

    🔧 Correct Proxy Config to only pass api requests to ddev

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,17 @@
 // vue.config.js
 module.exports = {
-  outputDir: '../public',
-  css: {
-    sourceMap: true
-  },
-  devServer: {
-    proxy: 'http://lartify.ddev.local'
-  },
-  indexPath: process.env.NODE_ENV === 'production'
-      ? '../resources/views/index.blade.php'
-      : 'index.html'
+    outputDir: '../public',
+    css: {
+        sourceMap: true
+    },
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://lartify.ddev.local'
+            }
+        }
+    },
+    indexPath: process.env.NODE_ENV === 'production'
+        ? '../resources/views/index.blade.php'
+        : 'index.html'
 };
